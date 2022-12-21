25c63f13e49037b58aa252328989993bd7489353
Art2berg <alexeyartberg@gmail.com>
2022-12-21 23:15:17 +0200

    centered iframe, fix iframe size

---
+++ nuxt.config.js
@@ -0,0 +1,31 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "Unity WebGL Player | MappaTest",
+    htmlAttrs: {
+      lang: "en-us",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { "http-equiv": "Content-Type", content: "text/html; charset=utf-8" },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["~static/css/style.css"],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="text_center">Mappa WebGL test</h1>
+    <iframe
+      mozallowfullscreen="true"
+      allow="autoplay; fullscreen"
+      msallowfullscreen="true"
+      allowfullscreen
+      webkitallowfullscreen="true"
+      src="MapWebGL/index.html"
+      name="Mappa"
+      allowtransparency="true"
+      height="640px"
+      width="960px"
+    ></iframe>
+  </div>
+</template>
+
+<script></script>
